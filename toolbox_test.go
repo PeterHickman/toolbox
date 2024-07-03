15e5c9a705d391e037a23c90122ce0ae0fd01d77
Peter Hickman <peterhickman386@googlemail.com>
2024-07-03 23:12:07 +0100

    test, sorta

---
+++ toolbox_test.go
@@ -0,0 +1,21 @@
+package toolbox
+
+import (
+	"testing"
+)
+
+func TestFileNotExists(t *testing.T) {
+	if FileExists("dummy.txt") {
+		t.Fatal("dummy.txt should not exist")
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	if !FileExists("toolbox.go") {
+		t.Fatal("toolbox.go does exist")
+	}
+}
+
+func TestCommand(t *testing.T) {
+	Command("ls -lh /")
+}
