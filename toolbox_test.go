53e97de628cefc4fc783056f40bc591766c853b5
Peter Hickman <peterhickman386@googlemail.com>
2024-07-14 20:33:34 +0100

    broken tests huh

--- toolbox_test.go
+++ toolbox_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-const valid_sha string = "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-const valid_md5 string = "md5:d41d8cd98f00b204e9800998ecf8427e"
+const valid_sha string = "sha256:a826c7e389ec9f379cafdc544d7e9a4395ff7bfb58917bbebee51b3d0b1c996a"
+const valid_md5 string = "md5:78745dd27ccc2f660afba9841f58259b"
 
 func create_file(filename, data string) {
 	d1 := []byte(data)
@@ -37,20 +37,22 @@ func TestCommandOutput(t *testing.T) {
 
 func TestCalculateMD5(t *testing.T) {
 	create_file("tmp.txt", "This is a message")
-	os.Remove("tmp.txt")
 
 	crc := CalculateMD5("tmp.txt")
 	if crc != valid_md5 {
 		t.Fatal("MD5 should be " + valid_md5 + " is " + crc)
 	}
+
+	os.Remove("tmp.txt")
 }
 
 func TestCalculateSHA256(t *testing.T) {
 	create_file("tmp.txt", "This is a message")
-	os.Remove("tmp.txt")
 
 	crc := CalculateSHA256("tmp.txt")
 	if crc != valid_sha {
 		t.Fatal("SHA256 should be " + valid_sha + " is " + crc)
 	}
+
+	os.Remove("tmp.txt")
 }
