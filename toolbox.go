bef24a66d9f511afb8789e6de093b9621a12dfd3
Peter Hickman <peterhickman386@googlemail.com>
2024-07-14 16:43:25 +0100

    these also appear more than once

--- toolbox.go
+++ toolbox.go
@@ -1,11 +1,38 @@
 package toolbox
 
 import (
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
 	"errors"
+	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+func CalculateMD5(filename string) string {
+	var file, _ = os.Open(filename)
+	defer file.Close()
+
+	var hash = md5.New()
+	io.Copy(hash, file)
+
+	var bytesHash = hash.Sum(nil)
+	return fmt.Sprintf("md5:%s", hex.EncodeToString(bytesHash[:]))
+}
+
+func CalculateSHA256(filename string) string {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	hash := sha256.New()
+	io.Copy(hash, file)
+
+	var bytesHash = hash.Sum(nil)
+	return fmt.Sprintf("sha256:%s", hex.EncodeToString(bytesHash[:]))
+}
 
 func FileExists(filename string) bool {
 	ok := true
--- toolbox_test.go
+++ toolbox_test.go
@@ -1,8 +1,17 @@
 package toolbox
 
 import (
+	"os"
 	"testing"
 )
+
+const valid_sha string = "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+const valid_md5 string = "md5:d41d8cd98f00b204e9800998ecf8427e"
+
+func create_file(filename, data string) {
+	d1 := []byte(data)
+	os.WriteFile(filename, d1, 0644)
+}
 
 func TestFileNotExists(t *testing.T) {
 	if FileExists("dummy.txt") {
@@ -24,4 +33,24 @@ func TestCommandOutput(t *testing.T) {
 	o, _ := CommandOutput("ls -lh /")
 
 	println(o)
+}
+
+func TestCalculateMD5(t *testing.T) {
+	create_file("tmp.txt", "This is a message")
+	os.Remove("tmp.txt")
+
+	crc := CalculateMD5("tmp.txt")
+	if crc != valid_md5 {
+		t.Fatal("MD5 should be " + valid_md5 + " is " + crc)
+	}
+}
+
+func TestCalculateSHA256(t *testing.T) {
+	create_file("tmp.txt", "This is a message")
+	os.Remove("tmp.txt")
+
+	crc := CalculateSHA256("tmp.txt")
+	if crc != valid_sha {
+		t.Fatal("SHA256 should be " + valid_sha + " is " + crc)
+	}
 }
